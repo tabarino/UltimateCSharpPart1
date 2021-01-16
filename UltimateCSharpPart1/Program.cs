a37483b254383891de15fe9f4b9fe7d16976e6cd
Ivan Tabarino <tabarino@outlook.com>
2021-01-16 23:29:28 +0000

    Create Project

---
+++ UltimateCSharpPart1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UltimateCSharpPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
